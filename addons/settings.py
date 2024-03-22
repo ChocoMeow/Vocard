9016a78125baf78b1dbccc32f82434e10f983b5a
Choco <94597336+ChocoMeow@users.noreply.github.com>
2024-03-22 11:28:58 +0800

    Added some typehint

--- addons/settings.py
+++ addons/settings.py
@@ -3,25 +3,27 @@ import os
 from dotenv import load_dotenv
 from typing import (
     Dict,
-    List
+    List,
+    Any,
+    Union
 )
 
 class Settings:
     def __init__(self, settings: Dict) -> None:
         self.invite_link: str = "https://discord.gg/wRCgB7vBQv"
-        self.nodes: Dict = settings.get("nodes", {})
+        self.nodes: Dict[str, Dict[str, Union[str, int, bool]]] = settings.get("nodes", {})
         self.max_queue: int = settings.get("default_max_queue", 1000)
         self.bot_prefix: str = settings.get("prefix", "")
-        self.activity: Dict = settings.get("activity", [{"listen": "/help"}])
-        self.logging: Dict = settings.get("logging", {})
+        self.activity: List[Dict[str, str]] = settings.get("activity", [{"listen": "/help"}])
+        self.logging: Dict[Union[str, Dict[str, Union[str, bool]]]] = settings.get("logging", {})
         self.embed_color: str = int(settings.get("embed_color", "0xb3b3b3"), 16)
-        self.bot_access_user: List = settings.get("bot_access_user", [])
-        self.sources_settings: Dict = settings.get("sources_settings", {})
-        self.cooldowns_settings: Dict = settings.get("cooldowns", {})
-        self.aliases_settings: Dict = settings.get("aliases", {})
-        self.controller: Dict = settings.get("default_controller", {})
+        self.bot_access_user: List[int] = settings.get("bot_access_user", [])
+        self.sources_settings: Dict[Dict[str, str]] = settings.get("sources_settings", {})
+        self.cooldowns_settings: Dict[str, List[int]] = settings.get("cooldowns", {})
+        self.aliases_settings: Dict[str, List[str]] = settings.get("aliases", {})
+        self.controller: Dict[str, Dict[str, Any]] = settings.get("default_controller", {})
         self.lyrics_platform: str = settings.get("lyrics_platform", "A_ZLyrics").lower()
-        self.ipc_server: Dict = settings.get("ipc_server", {})
+        self.ipc_server: Dict[str, Union[str, bool, int]] = settings.get("ipc_server", {})
         self.version: str = settings.get("version", "")
 
 class TOKENS:
