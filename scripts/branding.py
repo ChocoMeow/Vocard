8fc388fc3f06f6a425fff99c9b2ad4b3c79b1234
Nischay Sharma <nischay876@gmail.com>
2025-10-03 18:17:30 +0530

    Update branding.py

--- scripts/branding.py
+++ scripts/branding.py
@@ -7,8 +7,8 @@ replacements = [
     {"from": "Vocard", "to": "MeCute"},
     {"from": "vocard.xyz", "to": "mecute.bot.nu"},
     {"from": "wRCgB7vBQv", "to": "NYySajyC3G"},
-    {"from": "https://ko-fi.com/chocoo", "to": "https://ko-fi.com/mecute"},
-    {"from": "https://www.patreon.com/Vocard", "to": "https://www.patreon.com/mecute"},
+    {"from": "https://ko-fi.com/chocoo", "to": "https://github.com/sponsors/nischay876"},
+    {"from": "https://www.patreon.com/Vocard", "to": "https://github.com/sponsors/nischay876"},
     {"from": "https://www.termsfeed.com/live/4322db80-d6f4-4cd0-9aaf-73080323ff01", "to": "https://nischay876.github.io/MeCute-Legal/Privacy_Policy.html"},
     {"from": "https://www.termsfeed.com/live/4d3977eb-65b6-4ce2-a446-1cd80e619ab0", "to": "https://nischay876.github.io/MeCute-Legal/Terms_of_Service.html"}
 ]
